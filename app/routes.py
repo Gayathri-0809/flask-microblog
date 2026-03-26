495d81ccb7bd343dc6f9dae3e04ca81c0ceb036d
Gayathri R <gayathriramakrishnan2005@gmail.com>
2026-03-26 20:17:09 +0530

    Implemented user login form using Flask-WTF with validation,CSRF protection, and routing

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Gayathri'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Madurai!'
-        },
-        {
-            'author': {'username': 'Priya'},
-            'body': 'Learning Flask is fun!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form)
